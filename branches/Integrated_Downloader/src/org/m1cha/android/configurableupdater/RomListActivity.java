4a0fa2fba21fb55f303cd67ae90a07fb9e8345c2
custofreak@googlemail.com <custofreak@googlemail.com@fe4c2903-171e-5f11-a0db-6f05c051f39a>
2011-09-24 06:09:25 +0000

    * started implementing romlist-tab

--- branches/Integrated_Downloader/src/org/m1cha/android/configurableupdater/RomListActivity.java
+++ branches/Integrated_Downloader/src/org/m1cha/android/configurableupdater/RomListActivity.java
@@ -1,15 +1,26 @@
 package org.m1cha.android.configurableupdater;
 
+import org.m1cha.android.configurableupdater.romtools.RomList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class RomListActivity extends Activity {
 
 	private MainActivity ma;
+	public RomList romList;
+	private String[] romNames;
+	private Spinner spinner_roms;
+	private String changelog;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,28 @@ public class RomListActivity extends Activity {
 		
 		/** set layout */
 		setContentView(R.layout.rom_selection);
+		
+		/** get romList */
+		this.romList = new RomList(this, getString(R.string.default_romFolder));
+		this.romNames = this.romList.getRomNames();
+		
+		/** add roms to spinner */
+		ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, romNames);
+		this.spinner_roms = (Spinner)findViewById(R.id.rom_selection_buttonRoms);
+		this.spinner_roms.setAdapter(spinnerArrayAdapter);
+		
+		/** onclick-Listener for spinner */
+		this.spinner_roms.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> av, View v, int index, long l_index) {
+				RomSelection_setRom(index);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
 		
 	}
 	
@@ -47,5 +80,31 @@ public class RomListActivity extends Activity {
 		Util.menuHandler(this, item);
     	return super.onOptionsItemSelected(item);
     }
+	
+	public void RomSelection_setRom(int index) {
+		
+		/** set cover */
+		ImageView myImage = (ImageView) findViewById(R.id.rom_selection_cover);
+		if(this.romList.getRom(index).getCover()!=null) {
+			myImage.setImageBitmap(this.romList.getRom(index).getCover());
+		}
+		else {
+			myImage.setImageResource(R.drawable.nocover);
+		}
+		
+		/** set changelog */
+		this.changelog = this.romList.getRom(index).getChangelog();
+		if(changelog.length()>0) {
+			findViewById(R.id.rom_selection_buttonChangelog).setEnabled(true);
+		}
+		else {
+			findViewById(R.id.rom_selection_buttonChangelog).setEnabled(false);
+		}
+		
+		/** set current ROM */
+		DataStore.currentRom = this.romList.getRom(index);
+		findViewById(R.id.rom_selection_buttonNext).setEnabled(true);
+        
+	}
 
 }
