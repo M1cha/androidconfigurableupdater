e07e9b997bfe00d674f604143f3f78d6f0a69d42
custofreak <custofreak@googlemail.com>
2011-10-11 18:28:02 +0000

    * localizes TabControl * removed unused imports in DataStore

--- src/org/m1cha/android/configurableupdater/DataStore.java
+++ src/org/m1cha/android/configurableupdater/DataStore.java
@@ -3,8 +3,6 @@ package org.m1cha.android.configurableupdater;
 import org.json.JSONObject;
 import org.m1cha.android.configurableupdater.romtools.RomObject;
 
-import android.content.Context;
-
 public class DataStore {
 	public static RomObject currentRom;
 	public static JSONObject advancedSettings;
